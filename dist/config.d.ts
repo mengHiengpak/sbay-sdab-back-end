5ad10630c6fcc7f5615e62916ea7018641454f5d
Pak Menghieng <menghiengpak096@gmail.com>
2026-05-30 17:33:18 +0700

    Stop tracking dist/ and bin/ in git, reduce repo size

--- dist/config.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function setYtDlpPath(path: string | undefined): void;
-export declare function getYtDlpPath(): string | undefined;
-//# sourceMappingURL=config.d.ts.map
--- dist/middleware/auth.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-declare const auth: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export default auth;
-//# sourceMappingURL=auth.d.ts.map
